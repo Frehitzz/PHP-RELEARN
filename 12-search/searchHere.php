a773bf4b0443e6a803779aa4360f75f70269854a
Frehitzz <fritzharlydegamo@gmail.com>
2026-02-03 23:29:07 +0800

    style: add search page

--- 12-search/searchHere.php
+++ 12-search/searchHere.php
@@ -3,9 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Search</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Hello</h1>
+     <form action="" method="">
+        <div class="container min-vh-100" style="max-width: 500px;">
+            <!-- SEARCH INPUT -->
+            <div class="input-group p-5">
+                <input class="form-control" type="text" placeholder="Search here">
+                <button class="btn btn-primary border-0"><i class="bi bi-search"></i></button>
+            </div>
+
+            <!-- DISPLAY SEARCH ITEM  -->
+            <div class="">
+                <ul>
+                    <li></li>  
+                </ul>
+            </div>
+        </div>
+     </form>
+    
+
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
